da047ec72f90c56ac71aa1f7237eb2fdf4dddcb5
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-27 18:38:44 -0300

    feat: Course repository interface

---
+++ src/Domain/Repositories/ICourseRepository.cs
@@ -0,0 +1,13 @@
+using enova_academy.Domain.Entities;
+
+namespace enova_academy.Domain.Repositories;
+
+public interface ICourseRepository
+{
+    Task AddAsync(Course course);
+    Task<Course?> GetByIdAsync(Guid id);
+    Task<List<Course>> GetAllAsync();
+    IQueryable<Course> Query();
+    Task DeleteAsync(Course course);
+    Task SaveChangesAsync();
+}
