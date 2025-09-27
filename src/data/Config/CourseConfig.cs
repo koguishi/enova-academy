58d317e763e13c48ce6eb13fe025415735df696b
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-27 18:41:38 -0300

    feat: CourseConfig para EF

---
+++ src/data/Config/CourseConfig.cs
@@ -0,0 +1,31 @@
+using enova_academy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace enova_academy.Data.Config;
+
+public class CourseConfig : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Title)
+            .HasColumnType("varchar")
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(c => c.Slug)
+            .HasColumnType("varchar")
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(m => m.Price)
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(m => m.Capacity)
+            .HasColumnType("integer");
+
+        builder.ToTable("Courses");
+    }
+}
