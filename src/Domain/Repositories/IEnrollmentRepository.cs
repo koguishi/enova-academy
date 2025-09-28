a9d2957103bcb7ab13535ff2a18129c06a427dcb
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-28 09:12:19 -0300

    feat: Enrollment repository interface

---
+++ src/Domain/Repositories/IEnrollmentRepository.cs
@@ -0,0 +1,12 @@
+using enova_academy.Domain.Entities;
+
+namespace enova_academy.Domain.Repositories;
+
+public interface IEnrollmentRepository
+{
+    Task AddAsync(Enrollment enrollment);
+    Task<Enrollment?> GetByIdAsync(Guid id);
+    IQueryable<Enrollment> Query();
+    Task DeleteAsync(Enrollment enrollment);
+    Task SaveChangesAsync();
+}
