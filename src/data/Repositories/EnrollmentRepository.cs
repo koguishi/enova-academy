8d65869b317a767211bdb5f8ae276b44a3e391cc
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-28 09:13:48 -0300

    feat: Enrollment repository

---
+++ src/data/Repositories/EnrollmentRepository.cs
@@ -0,0 +1,44 @@
+using enova_academy.Domain.Entities;
+using enova_academy.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace enova_academy.Data.Repositories;
+
+public class EnrollmentRepository : IEnrollmentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public EnrollmentRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Enrollment enrollment)
+    {
+        await _context.Enrollments.AddAsync(enrollment);
+    }
+
+    public async Task<Enrollment?> GetByIdAsync(Guid id)
+    {
+        var enrollment = await _context.Enrollments
+            .Where(p => p.Id == id)
+            .FirstOrDefaultAsync();
+        return enrollment;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(Enrollment enrollment)
+    {
+        _context.Enrollments.Remove(enrollment);
+        return _context.SaveChangesAsync();
+    }
+
+    IQueryable<Enrollment> IEnrollmentRepository.Query()
+    {
+        return _context.Enrollments.AsQueryable();
+    }
+}
