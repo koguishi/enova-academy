1962581b88dc7243c13f8dbac400b87980d18260
Edson Koguishi <edsonkoguishi@gmail.com>
2025-09-29 15:17:25 -0300

    test: moq de cache

--- tests/enova-academy.UnitTests/Application/CourseServiceTests.cs
+++ tests/enova-academy.UnitTests/Application/CourseServiceTests.cs
@@ -1,11 +1,9 @@
-using Xunit;
 using Moq;
-using System;
-using System.Threading.Tasks;
 using enova_academy.Domain.Repositories;
 using enova_academy.Domain.Entities;
 using enova_academy.Application.Services;
 using enova_academy.Application.DTOs;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace MyApp.UnitTests.Application
 {
@@ -17,9 +15,12 @@ namespace MyApp.UnitTests.Application
             // Arrange
             var repoMock = new Mock<ICourseRepository>();
             repoMock.Setup(r => r.GetBySlugAsync("dotnet", false))
-                    .ReturnsAsync(new Course("Existente", "dotnet", 100m, 10));
+                .ReturnsAsync(new Course("Existente", "dotnet", 100m, 10)
+            );
 
-            var service = new CourseService(repoMock.Object);
+            var cacheMock = new Mock<IDistributedCache>();                    
+
+            var service = new CourseService(repoMock.Object, cacheMock.Object);
 
             // Ajuste a construção do DTO conforme o seu DTO real (construtor/props)
             var dto = new CourseDto { Title = "Novo", Slug = "dotnet", Price = 200m, Capacity = 30 };
